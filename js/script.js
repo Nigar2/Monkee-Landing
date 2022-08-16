c14506c0b33b9e62ceb5d90e9b6807e1d2ad7d6f
Nigar2 <102859043+Nigar2@users.noreply.github.com>
2022-08-16 14:34:22 +0400

    monkee

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+//   $(window).on('load', function() { 
+//   $('#status').fadeOut(); 
+//   $('#preloader').delay(350).fadeOut('slow');
+//   $('body').delay(350).css({'overflow':'visible'});
+// })
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+  loader.style.display="none"
+  
+})
